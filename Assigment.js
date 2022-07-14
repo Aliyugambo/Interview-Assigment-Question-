93ab49e3e24e2bd1a5b7d777b3e12a62d0ff8086
Aliyugambo <agathinktwice123@gmail.com>
2022-07-15 00:10:06 +0100

    Assigment Question

---
+++ Assigment.js
@@ -0,0 +1,15 @@
+// A function that returns the two numbers such that they add up to target.
+let nums = [2, 7, 11, 15];
+
+let twoSum = function(nums, target) {
+    let maps = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let num1 = nums[i];
+        let num2 = target - num1;
+        if (maps.has(num2)) {
+            return [nums[i], num2];
+        }
+        maps.set(num1, i);
+    }
+};
+console.log(twoSum(nums, 9));
