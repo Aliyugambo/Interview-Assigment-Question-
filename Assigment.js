c31c676b8e7e5f666039bd1f95a9dc217da45ec3
Aliyu Gambo Aliyu <gamboaliyu00@gmail.com>
2022-10-23 15:25:45 +0100

    ADD:UPDATES

--- Assigment.js
+++ Assigment.js
@@ -1,5 +1,6 @@
 let nums = [2, 7, 11, 15];
 
+// PRINTING OUT THE ACTUAL VALUE
 let twoSum = function(nums, target) {
     let maps = new Map();
     for (let i = 0; i < nums.length; i++) {
@@ -12,3 +13,17 @@ let twoSum = function(nums, target) {
     }
 };
 console.log(twoSum(nums, 9));
+
+// PRINTING OUT THE INDICE OF THE VALUES
+
+// var twoSum = function(nums, target) {
+//     let maps = new Map();
+//     for (let i = 0; i < nums.length; i++) {
+//         // let num1 = nums[i];
+//         let num2 = target - nums[i];
+//         if (maps.has(num2)) {
+//             return [maps.get(num2), i];
+//         }
+//         maps.set(nums[i], i);
+//     }
+// };
